c8ecb6f927a9463633d1137865cfe9fc64d59873
Cory Paszul <cpaszul@gmail.com>
2020-12-08 12:27:29 -0500

    Improved

--- day8.py
+++ day8.py
@@ -2,38 +2,30 @@ DEFAULT_INPUT = 'day8.txt'
 
 def part_1(loc: str = DEFAULT_INPUT) -> int:
     with open(loc) as f:
-        lines = [((ln := line.rstrip().split(' '))[0], int(ln[1])) for line in f.readlines()]
-    seen = set()
-    acc = 0
-    cur = 0
-    while True:
-        if cur in seen:
-            return acc
-        seen.add(cur)
-        op, val = lines[cur]
-        if op == 'acc':
-            acc += val
-        cur += val if op == 'jmp' else 1
+        lines = [((ln := line.rstrip().split(' '))[0], int(ln[1]))
+                 for line in f.readlines()]
+    return run(lines)[1]
 
 def part_2(loc: str = DEFAULT_INPUT) -> int:
     with open(loc) as f:
-        lines = [((ln := line.rstrip().split(' '))[0], int(ln[1])) for line in f.readlines()]
+        lines = [((ln := line.rstrip().split(' '))[0], int(ln[1]))
+                 for line in f.readlines()]
     for i in range(len(lines)):
         op, val = lines[i]
         if op in ('nop', 'jmp'):
             new_lines = lines.copy()
             new_op = 'nop' if op == 'jmp' else 'jmp'
             new_lines[i] = (new_op, val)
-            if (res := finishes(new_lines))[0]:
+            if (res := run(new_lines))[0]:
                 return res[1]
                     
-def finishes(lines: list[tuple[str, int]]) -> tuple[bool, int]:
+def run(lines: list[tuple[str, int]]) -> tuple[bool, int]:
     seen = set()
     acc = 0
     cur = 0
     while cur < len(lines):
         if cur in seen:
-            return False, 0
+            return False, acc
         seen.add(cur)
         op, val = lines[cur]
         if op == 'acc':
